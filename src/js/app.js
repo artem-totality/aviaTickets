3d550417a80fec6c130ea697dba125d1fb426a81
artem-totality <artem.totality@gmail.com>
2021-02-17 23:24:22 +0200

    intermediate commit

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,7 @@ import "../css/style.css";
 import "./plugins";
 import locations from "./store/locations";
 
-// locations.init().then((res) => {
-//   console.log(res);
-//   console.log(locations);
-
-//   console.log(locations.getCitiesByCountryCode("PE"));
-// });
+locations.init().then((res) => {
+  console.log(res);
+  console.log(locations);
+});
--- src/js/store/locations.js
+++ src/js/store/locations.js
@@ -13,11 +13,39 @@ class Locations {
     ]);
 
     const [countries, cities] = response;
-    this.countries = countries;
-    this.cities = cities;
+    this.countries = this.serializeCountries(countries);
+    this.cities = this.serializeCities(cities);
 
     return response;
   }
+
+  createShortCitiesList(cities) {
+    // return cities;
+  }
+
+  serializeCountries(countries) {
+    // { 'Country code': { ... } }
+    return countries.reduce((acc, country) => {
+      acc[country.code] = country;
+      return acc;
+    }, {});
+  }
+
+  serializeCities(cities) {
+    // { 'City name, Country name': { ... }}
+    return cities.reduce((acc, city) => {
+      const country_name = this.getCountryNameByCode(city.country_code);
+      const city_name = city.name || city.name_translations.en;
+      const key = `${city_name},${country_name}`;
+      acc[key] = city;
+      return acc;
+    }, {});
+  }
+
+  getCountryNameByCode(code) {
+    return this.countries[code].name;
+  }
+
   getCitiesByCountryCode(code) {
     return this.cities.filter((city) => city.country_code === code);
   }
@@ -26,3 +54,8 @@ class Locations {
 const locations = new Locations(api);
 
 export default locations;
+
+// { 'City, Countrty': null }
+// [{}, {}]
+// { 'cityCode': {...} } => cities[code]
+// {}
